9139821a990bad9b1e46b6aa770efab1686c56b9
Lujain <lujain@dyn-172-20-97-128.qatar.cmu.local>
2024-01-25 12:29:14 +0300

    I added the base code that connects the firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { onSnapshot, collection } from '@firebase/firestore';
+import { useEffect, useState } from "react";
+import db from "./firebase";
+
 
 function App() {
+  const [ materials, setMaterials ] = useState([]);
+  //console from the state 
+  console.log(materials);
+  
+ 
+  
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, "document-1"), (snapshot) => {
+      
+      setMaterials(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+    // snapshot.docs.forEach((doc) => {
+
+        // console.log(`Document ID: ${doc.id}`);
+        // console.log(`Author: ${doc.data().Author}`);
+        // console.log(`Title: ${doc.data().Title}`);
+        // console.log(`URL: ${doc.data().URL}`);
+    //  });
+    });
+    return unsubscribe;
+  }, []);
+  
+   
+    
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <div className="materials-list">
+        {materials.map((material) => (
+          <div key={material.id} className="material">
+            <div><strong>ID:</strong> {material.id}</div>
+            <div><strong>Title:</strong> {material.Title}</div>
+            <div><strong>Author:</strong> {material.Author}</div>
+            <div><strong>URL:</strong> <a href={material.URL} target="_blank" rel="noopener noreferrer">{material.URL}</a></div>
+          </div>
+        ))}
+      </div>
     </div>
   );
+
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAOtvJUU_WbnXFr5f5q7pRtpkG0sabUVRY",
+  authDomain: "sdglibrary-dfc2c.firebaseapp.com",
+  projectId: "sdglibrary-dfc2c",
+  storageBucket: "sdglibrary-dfc2c.appspot.com",
+  messagingSenderId: "561580139043",
+  appId: "1:561580139043:web:1706a5e6569a65226c0796"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//export default getFirestore();
+
+
+// Initialize Firestore
+const db = getFirestore(app);
+
+export default db;
