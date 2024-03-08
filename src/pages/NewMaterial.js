68a06da5c8db6bf8fee8a5c857d48f54c8de45dd
LujainAlMansoori <lbs.13x@gmail.com>
2024-03-08 16:07:33 +0300

    Added new fields to the materials page and made items searchable

--- src/pages/NewMaterial.js
+++ src/pages/NewMaterial.js
@@ -6,7 +6,20 @@ import {
   getDownloadURL,
 } from "firebase/storage";
 import db, { storage } from "../firebase";
-import "../components/style/Form.css";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import MenuItem from "@mui/material/MenuItem";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
 export default function NewMaterial() {
   const [materialDetails, setMaterialDetails] = useState({
@@ -15,13 +28,22 @@ export default function NewMaterial() {
     date_published: "",
     description: "",
     link: "",
+    category: "",
+    publisher: "",
+    journalName: "",
+    institution: "",
+    tags: "", // Initialize tags as an empty string
   });
   const [file, setFile] = useState(null);
   const [inputKey, setInputKey] = useState(Date.now());
 
   const handleChange = (e) => {
     if (e.target.type === "file") {
-      setFile(e.target.files[0]); // Set the file
+      setFile(e.target.files[0]);
+      setMaterialDetails({
+        ...materialDetails,
+        fileName: e.target.files[0] ? e.target.files[0].name : "", // Update the fileName property
+      });
     } else {
       const { name, value } = e.target;
       setMaterialDetails({
@@ -30,37 +52,32 @@ export default function NewMaterial() {
       });
     }
   };
-
-  // Handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    // First, upload the file if it exists
     let fileUrl = "";
     if (file) {
       const fileStorageRef = storageRef(storage, `documents/${file.name}`);
       const uploadResult = await uploadBytes(fileStorageRef, file);
       fileUrl = await getDownloadURL(uploadResult.ref);
     }
-
-    // Then, add the document reference to the materialDetails object
     const finalMaterialDetails = {
       ...materialDetails,
-      documentUrl: fileUrl, // Include the file URL here
+      documentUrl: fileUrl,
     };
-
-    // Finally, add the new material details to Firestore
     try {
       await addDoc(collection(db, "materials"), finalMaterialDetails);
       console.log("Document successfully written!");
-
-      // Reset the form and file state
       setMaterialDetails({
         title: "",
         author: "",
         date_published: "",
         description: "",
         link: "",
+        category: "",
+        publisher: "",
+        journalName: "",
+        institution: "",
+        tags: "", // Reset tags to an empty string
       });
       setFile(null);
       setInputKey(Date.now());
@@ -70,70 +87,191 @@ export default function NewMaterial() {
   };
 
   return (
-    <div className="form-container">
-      <div className="title">New Material Page</div>
-      <form onSubmit={handleSubmit}>
-        <div className="form-titles"> Title</div>
-        <input
-          type="text"
-          name="title"
-          placeholder="Title"
-          value={materialDetails.title}
-          onChange={handleChange}
-          className="form-field"
-        />
-        <div className="form-titles"> Author</div>
-        <input
-          type="text"
-          name="author"
-          placeholder="Author"
-          value={materialDetails.author}
-          onChange={handleChange}
-          className="form-field"
-        />
-        <div className="form-titles"> Date Published</div>
-        <input
-          type="date"
-          name="date_published"
-          placeholder="Date Published"
-          value={materialDetails.date_published}
-          onChange={handleChange}
-          className="form-field"
-        />
-        <div className="form-titles"> Description</div>
-        <textarea
-          name="description"
-          placeholder="Description"
-          value={materialDetails.description}
-          onChange={handleChange}
-          className="form-field"
-        />
-        <div className="form-titles"> Link</div>
-        <input
-          type="text"
-          name="link"
-          placeholder="Link"
-          value={materialDetails.link}
-          onChange={handleChange}
-          className="form-field"
-        />
+    <Paper
+      elevation={4}
+      sx={{
+        mt: 7,
+        backgroundColor: "white",
+        width: 700,
+        marginLeft: "23%",
+        // height: 830,
+        mb: 4,
+        padding: 2,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Container component="main" maxWidth="sm">
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5" sx={{ mb: 3 }}>
+            New Material
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 1 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="title"
+                  label="Title"
+                  value={materialDetails.title}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="author"
+                  label="Author"
+                  value={materialDetails.author}
+                  onChange={handleChange}
+                />
+              </Grid>
 
-        <div className="form-titles"> File</div>
-        <input
-          type="file"
-          name="file_upload"
-          onChange={handleChange}
-          className="form-field"
-          style={{ justifyContent: "center" }}
-          key={inputKey}
-        />
+              {/* Category field */}
+              <Grid item xs={12}>
+                <FormControl fullWidth required>
+                  <InputLabel id="category-label">Category</InputLabel>
+                  <Select
+                    labelId="category-label"
+                    id="category"
+                    name="category"
+                    value={materialDetails.category}
+                    label="Category"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value="Journal">Journal</MenuItem>
+                    <MenuItem value="Research Paper">Research Paper</MenuItem>
+                    <MenuItem value="Lesson Plan">Lesson Plan</MenuItem>
+                    <MenuItem value="Annual Report">Annual Report</MenuItem>
+                    <MenuItem value="Academic Writing">
+                      Academic Writing
+                    </MenuItem>
+                    <MenuItem value="Report">Report</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              {/* Institution field */}
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="institution"
+                  label="Institution"
+                  value={materialDetails.institution}
+                  onChange={handleChange}
+                />
+              </Grid>
 
-        <div className="submit-button-container">
-          <button type="submit" className="submit-button">
-            Submit
-          </button>
-        </div>
-      </form>
-    </div>
+              {/* Publisher field */}
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="publisher"
+                  label="Publisher"
+                  value={materialDetails.publisher}
+                  onChange={handleChange}
+                />
+              </Grid>
+              {/* Journal Name field */}
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="journalName"
+                  label="Journal Name"
+                  value={materialDetails.journalName}
+                  onChange={handleChange}
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="date_published"
+                  label="Date Published"
+                  type="date"
+                  InputLabelProps={{ shrink: true }}
+                  value={materialDetails.date_published}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  multiline
+                  rows={4}
+                  name="description"
+                  label="Description"
+                  value={materialDetails.description}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="link"
+                  label="Link"
+                  value={materialDetails.link}
+                  onChange={handleChange}
+                />
+              </Grid>
+              {/* Upload File field */}
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  type="text"
+                  value={materialDetails.fileName || "Upload File"}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton color="primary" component="label">
+                          <CloudUploadIcon />
+                          <input
+                            type="file"
+                            name="file_upload"
+                            hidden
+                            onChange={handleChange}
+                            key={inputKey}
+                          />
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                  disabled
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Submit
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </Paper>
   );
 }
--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -1,7 +1,109 @@
+import React, { useState, useEffect } from "react";
+import { db } from "../firebase";
+import { collection, getDocs } from "firebase/firestore";
+import Paper from "@mui/material/Paper";
+import "../components/style/titles.css";
+import IconButton from "@mui/material/IconButton";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import "../components/style/noHover.css";
+import emailjs from "emailjs-com";
+import { useAuth } from "../contexts/AuthContexts";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { doc, getDoc } from "firebase/firestore";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Box from "@mui/material/Box";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
+import Modal from "@mui/material/Modal";
+
 export default function SearchResults() {
+  const [materials, setMaterials] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    const fetchMaterials = async () => {
+      const materialsCollection = collection(db, "materials");
+      const materialsSnapshot = await getDocs(materialsCollection);
+      const materialsList = materialsSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setMaterials(materialsList);
+    };
+
+    fetchMaterials();
+  }, []);
+
+  // Search by these attributes
+  const filteredMaterials = materials.filter((material) => {
+    const searchLower = searchQuery.toLowerCase();
+    return (
+      material.title.toLowerCase().includes(searchLower) ||
+      (material.author &&
+        material.author.toLowerCase().includes(searchLower)) ||
+      (material.category &&
+        material.category.toLowerCase().includes(searchLower)) ||
+      (material.date_published &&
+        material.date_published.toLowerCase().includes(searchLower)) ||
+      (material.description &&
+        material.description.toLowerCase().includes(searchLower)) ||
+      (material.institution &&
+        material.institution.toLowerCase().includes(searchLower)) ||
+      (material.journalName &&
+        material.journalName.toLowerCase().includes(searchLower)) ||
+      (material.publisher &&
+        material.publisher.toLowerCase().includes(searchLower)) ||
+      (material.tags && material.tags.toLowerCase().includes(searchLower))
+    );
+  });
+
   return (
     <div>
-      <h2> search page </h2>
+      <div className="flex flex-col items-center justify-center">
+        <p className="researchers-title">Materials</p>
+        <TextField
+          label="Search Materials..."
+          variant="outlined"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          style={{
+            margin: "10px 10px",
+            marginLeft: "40px",
+            width: 1250,
+          }}
+          sx={{
+            borderRadius: "20px",
+            "& .MuiOutlinedInput-root": {
+              borderRadius: "25px",
+            },
+          }}
+        />
+      </div>
+      <div style={{ display: "flex", flexWrap: "wrap", cursor: "pointer" }}>
+        {filteredMaterials.map((material, index) => (
+          <Paper
+            key={index}
+            elevation={3}
+            style={{
+              padding: "20px",
+              margin: "10px 10px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              width: 1400,
+              height: 100,
+            }}
+          >
+            <p style={{ fontWeight: "bold", fontSize: "18px" }}>
+              {material.title}
+            </p>
+          </Paper>
+        ))}
+      </div>
     </div>
   );
 }
