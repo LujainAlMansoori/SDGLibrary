03115155a07ba9c30fe8241ed2e54d6169ff2edb
MayarAlkurdi <mkurdi@andrew.cmu.edu>
2024-03-20 03:07:52 +0300

    started refactoring navbar

---
+++ src/components/NavBar copy.js
@@ -0,0 +1,153 @@
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
+import { useAuth } from "../contexts/AuthContexts";
+import logo from "./assets/logo.png";
+import { db } from "../firebase";
+import "./style/NavBar.css";
+import MenuListComposition from "./menuListComponent.js";
+import Modal from "@mui/material/Modal";
+import { Typography } from "@mui/material";
+
+export default function NavBar() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+
+  
+  const { currentUser, logout } = useAuth();
+
+  const [userProfile, setUserProfile] = useState(null);
+
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+      if (currentUser?.uid) {
+        const docRef = doc(db, "profiles", currentUser.uid);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          setUserProfile(docSnap.data());
+        } else {
+          console.log("No such document!");
+        }
+      }
+    };
+
+    fetchUserProfile();
+  }, [currentUser, location.pathname]);
+
+  const handleLogout = async () => {
+    try {
+      setError("");
+      await logout();
+      navigate("/signup");
+    } catch (error) {
+      console.error(error);
+      setError("Failed to Log Out.");
+    }
+  };
+
+  return (
+    <div style={{ width: "100%" }}>
+      <nav
+        className="navBar"
+        style={{
+          position: "fixed",
+          top: 0,
+          width: "100%",
+          zIndex: 1000,
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: "0 20px",
+        }}
+      >
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <Link to="/" exact>
+            <img
+              src={logo}
+              alt="Logo"
+              className="logo"
+              style={{ marginRight: "20px", marginLeft: "20px" }}
+            />
+          </Link>
+          <ul
+            style={{
+              display: "flex",
+              listStyle: "none",
+              padding: 0,
+              margin: 0,
+            }}
+          >
+            <li style={{ mmarginRight: "10px" }}>
+              <Link to="/">Home</Link>
+            </li>
+            {userProfile && userProfile.accountRole === "admin" && (
+              <li style={{ marginRight: "10px" }}>
+                <Link to="/NewMaterial">Add Material</Link>
+              </li>
+            )}
+            <li style={{ marginRight: "10px" }}>
+              <Link to="/SearchResults">Search</Link>
+            </li>
+            {currentUser &&
+              userProfile &&
+              userProfile.role &&
+              userProfile.firstName &&
+              userProfile.lastName && (
+                <li style={{ marginRight: "10px" }}>
+                  <Link to="/Researchers">Members</Link>
+                </li>
+              )}
+          </ul>
+        </div>
+        <div
+          style={{ display: "flex", alignItems: "center", marginRight: "40px" }}
+        >
+          {currentUser ? (
+            <>
+              {userProfile ? (
+                <>
+                  <img
+                    src={
+                      userProfile.profileImage ||
+                      require("../components/assets/profile-photo.webp")
+                    }
+                    alt="Profile"
+                    style={{
+                      width: "40px",
+                      height: "40px",
+                      border: "1px solid #5c5b5b",
+                      boxShadow: "0px 1px 1px rgba(0, 0, 0, 0.3)",
+                      borderRadius: "50%",
+                      marginRight: "10px",
+                    }}
+                  />
+                  <span style={{ marginRight: "10px" }}>
+                    {userProfile.title} {userProfile.firstName}{" "}
+                    {userProfile.lastName}
+                  </span>
+                  <MenuListComposition logout={logout} navigate={navigate} />{" "}
+                </>
+              ) : (
+                <Typography
+                  onClick={handleLogout}
+                  style={{ cursor: "pointer", marginRight: "10px" }}
+                >
+                  Log Out
+                </Typography>
+              )}
+            </>
+          ) : (
+            <Link
+              to="/Signup"
+              className="nav-link"
+              style={{ marginRight: "10px" }}
+            >
+              Sign In
+            </Link>
+          )}
+        </div>
+      </nav>
+    </div>
+  );
+}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,17 +4,32 @@ import { doc, getDoc } from "firebase/firestore";
 import { useAuth } from "../contexts/AuthContexts";
 import logo from "./assets/logo.png";
 import { db } from "../firebase";
-import "./style/NavBar.css";
 import MenuListComposition from "./menuListComponent.js";
-import Modal from "@mui/material/Modal";
-import { Typography } from "@mui/material";
+import {
+  Navbar,
+  Collapse,
+  Typography,
+  IconButton,
+} from "@material-tailwind/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
-export default function NavBar() {
+function NavItem({ label, link }) {
+  return (
+    <Link to={link}>
+      <Typography as="li" variant="small" className="p-1 font-medium text-black">
+        {label}
+      </Typography>
+    </Link>
+  );
+}
+
+function NavbarSimple() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen((cur) => !cur);
   const location = useLocation();
   const navigate = useNavigate();
   const [error, setError] = useState("");
 
-  
   const { currentUser, logout } = useAuth();
 
   const [userProfile, setUserProfile] = useState(null);
@@ -46,64 +61,39 @@ export default function NavBar() {
     }
   };
 
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpen(false)
+    );
+  }, []);
+
   return (
-    <div style={{ width: "100%" }}>
-      <nav
-        className="navBar"
-        style={{
-          position: "fixed",
-          top: 0,
-          width: "100%",
-          zIndex: 1000,
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          padding: "0 20px",
-        }}
-      >
-        <div style={{ display: "flex", alignItems: "center" }}>
+    <Navbar fullWidth className="bg-hbkuBlue border-none">
+      <div className="container mx-auto flex items-center justify-between">
+        <div className="flex gap-2">
           <Link to="/" exact>
-            <img
-              src={logo}
-              alt="Logo"
-              className="logo"
-              style={{ marginRight: "20px", marginLeft: "20px" }}
-            />
+            <img src={logo} alt="logo" className="h-8 lg:h-12 cursor-pointer" />
           </Link>
-          <ul
-            style={{
-              display: "flex",
-              listStyle: "none",
-              padding: 0,
-              margin: 0,
-            }}
-          >
-            <li style={{ mmarginRight: "10px" }}>
-              <Link to="/">Home</Link>
-            </li>
-            {userProfile && userProfile.accountRole === "admin" && (
-              <li style={{ marginRight: "10px" }}>
-                <Link to="/NewMaterial">Add Material</Link>
-              </li>
-            )}
-            <li style={{ marginRight: "10px" }}>
-              <Link to="/SearchResults">Search</Link>
-            </li>
-            {currentUser &&
-              userProfile &&
-              userProfile.role &&
-              userProfile.firstName &&
-              userProfile.lastName && (
-                <li style={{ marginRight: "10px" }}>
-                  <Link to="/Researchers">Members</Link>
-                </li>
+          <div className="hidden lg:block">
+            <ul className="mb-4 mt-2 flex flex-col gap-3 lg:mb-0 lg:mt-2 lg:flex-row lg:items-center lg:gap-8">
+              <NavItem label="Home" link="/" />
+              {userProfile && userProfile.accountRole === "admin" && (
+                <NavItem label="Add Material" link="/NewMaterial" />
               )}
-          </ul>
+              <NavItem label="Search" link="/SearchResults" />
+              {currentUser &&
+                userProfile &&
+                userProfile.role &&
+                userProfile.firstName &&
+                userProfile.lastName && (
+                  <NavItem label="Members" link="/Researchers" />
+                )}
+            </ul>
+          </div>
         </div>
-        <div
-          style={{ display: "flex", alignItems: "center", marginRight: "40px" }}
-        >
-          {currentUser ? (
+        <div className="lg:flex hidden items-center gap-2">
+        {currentUser ? (
             <>
               {userProfile ? (
                 <>
@@ -147,7 +137,82 @@ export default function NavBar() {
             </Link>
           )}
         </div>
-      </nav>
-    </div>
+        <IconButton
+          size="sm"
+          variant="text"
+          color="gray"
+          onClick={handleOpen}
+          className="ml-auto inline-block lg:hidden"
+        >
+          {open ? (
+            <XMarkIcon className="h-6 w-6" strokeWidth={2} />
+          ) : (
+            <Bars3Icon className="h-6 w-6" strokeWidth={2} />
+          )}
+        </IconButton>
+      </div>
+      <Collapse open={open}>
+        <ul className="mb-4 mt-2 flex flex-col gap-3 lg:mb-0 lg:mt-2 lg:flex-row lg:items-center lg:gap-8">
+        <NavItem label="Home" link="/" />
+              {userProfile && userProfile.accountRole === "admin" && (
+                <NavItem label="Add Material" link="/NewMaterial" />
+              )}
+              <NavItem label="Search" link="/SearchResults" />
+              {currentUser &&
+                userProfile &&
+                userProfile.role &&
+                userProfile.firstName &&
+                userProfile.lastName && (
+                  <NavItem label="Members" link="/Researchers" />
+                )}
+        </ul>
+        
+        {currentUser ? (
+            <>
+              {userProfile ? (
+                <>
+                  <img
+                    src={
+                      userProfile.profileImage ||
+                      require("../components/assets/profile-photo.webp")
+                    }
+                    alt="Profile"
+                    style={{
+                      width: "40px",
+                      height: "40px",
+                      border: "1px solid #5c5b5b",
+                      boxShadow: "0px 1px 1px rgba(0, 0, 0, 0.3)",
+                      borderRadius: "50%",
+                      marginRight: "10px",
+                    }}
+                  />
+                  <span style={{ marginRight: "10px" }}>
+                    {userProfile.title} {userProfile.firstName}{" "}
+                    {userProfile.lastName}
+                  </span>
+                  <MenuListComposition logout={logout} navigate={navigate} />{" "}
+                </>
+              ) : (
+                <Typography
+                  onClick={handleLogout}
+                  style={{ cursor: "pointer", marginRight: "10px" }}
+                >
+                  Log Out
+                </Typography>
+              )}
+            </>
+          ) : (
+            <Link
+              to="/Signup"
+              className="nav-link"
+              style={{ marginRight: "10px" }}
+            >
+              Sign In
+            </Link>
+          )}
+      </Collapse>
+    </Navbar>
   );
 }
+
+export default NavbarSimple;
